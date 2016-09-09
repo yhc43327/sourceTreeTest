8d110f765a5e3e5f426a589a4357cf9920e30be6
魏根 <445192461@qq.com>
2016-09-09 16:29:45 +0800

    初始化项目

---
+++ hehe/hehe/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hehe
+//
+//  Created by 魏根 on 16/9/9.
+//  Copyright © 2016年 魏根. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
